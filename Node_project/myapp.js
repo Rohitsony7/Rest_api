128f48e8447b5a73ab742fa5f1e3b09671e61d5e
Ronny7 <Soni.rohit.721@gmail.com>
2019-02-27 10:27:55 +0530

    Rest api called via GET method

---
+++ Node_project/myapp.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+var path = require('path')
+var ejs = require('ejs')
+var content = require('./data.json') //this we have to do
+const port = 4200
+/* Basic routing
+// Routing refers to determining how an application responds to a client request to a particular endpoint, which is a URI (or path) and a specific HTTP request method (GET, POST, and so on).
+
+// Each route can have one or more handler functions, which are executed when the route is matched.
+
+// Route definition takes the following structure:
+
+ app.METHOD(PATH, HANDLER) */
+
+//  app.all('/secret' ,function (req, res, next) {
+//     console.log('Accessing the secret section ...')
+//     next() // pass control to the next handler
+//     console.log(content)
+//     res.send('Hello world')
+//   })
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('index', {'content': content}))
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
